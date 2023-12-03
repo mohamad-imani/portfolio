bea680ca37eda545cd5cbbc6f635e2c276862d3c
mohamad-imani <imani.mohamad98@gmail.com>
2023-12-03 12:47:42 +0330

    change import method

--- src/components/Avatarr.jsx
+++ src/components/Avatarr.jsx
@@ -2,7 +2,7 @@ const Avatar = () => {
   return (
     <div className="hidden xl:flex xl:max-w-none">
       <img
-        src="../../public/half.png"
+        src={"/half.png"}
         width="1000px"
         height="600px"
         alt=""
